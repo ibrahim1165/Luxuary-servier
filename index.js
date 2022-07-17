61862cf169c7d53bc7ba96b6b23fbedaeb9efbbf
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-17 10:38:13 +0600

    herok log in

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 
 const uri = `mongodb+srv://${process.env.USER_BD}:${process.env.PASS_BD}@cluster0.5r5py.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +23,8 @@ async function run() {
 
     await client.connect();
     const serviceCollection = client.db('luxuryLiving').collection('service');
+    const orderCollection = client.db('luxuryLiving').collection('order');
+
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -30,12 +32,25 @@ async function run() {
       const result = await curser.toArray()
       res.send(result);
     });
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+  });
 
-    app.post('/service', async (req, res) => {
-      const newPost = req.body;
-      const result = await serviceCollection.insertOne(newPost);
-      res.send(result);
-    })
+  
+  app.post('/order',async (req, res) => {
+    const order = req.body;
+    const result = await orderCollection.insertOne(order);
+    res.send(result);
+  });
+  app.get('/order', async (req, res) => {
+    const query = {};
+    const curser = orderCollection.find(query);
+    const result = await curser.toArray()
+    res.send(result);
+  });
 
   }
   finally {
